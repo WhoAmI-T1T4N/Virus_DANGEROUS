d29b6b6d61b9c9d4172f0b0e6298f5a61cceb499
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-11-09 19:35:57 +0700

    Update Zink-Virus.sh

--- Zink-Virus.sh
+++ Zink-Virus.sh
@@ -16,6 +16,7 @@ echo $yellow "1). Link-Virus"
 echo $yellow "2). Link-GC"
 echo $yellow "3). Exit"
 read -p "Jangan Lupa Pilih :" santai
+
 if [ $santai = "1" ]
 then
 echo $black "===================================="
@@ -25,6 +26,7 @@ echo $green "3). Link Virus PC"
 echo $black "====================================="
 read -p "Pilih Link Virusnya :" saha
 fi
+
 if [ $saha = "1" ]
 then
 echo $yellow "Sedangg Menunggu....."
@@ -32,6 +34,7 @@ echo $yellow "Sipp, Maaf Menunggu..."
 echo $yellow "Link : https://www.mediafire.com/file/btffebdhkry46s0"
 echo $yellow "Maaf, Telah Menunggu"
 fi
+
 if [ $saha = "2" ]
 then
 echo $yellow "Sedang Menunggu....."
@@ -39,6 +42,7 @@ echo $yellow "Sipp, Maaf Menunggu..."
 echo $yellow "Link : https://www.mediafire.com/file/kiwpmbv0cxsvqhu/
 echo $yellow "Maaf, Telah Menunggu"
 fi
+
 if [ $saha = "3" ]
 then
 echo $yellow "Sedang, Menunggu......"
@@ -46,12 +50,14 @@ echo $yellow "Sipp, Maaf Menunggu..."
 echo $yellow "Link : https://www.mediafire.com/file/btffebdhkry46s0"
 echo $yellow "Maaf, Menunggu..."
 fi
+
 if [ $santai = "2" ]
 then
 echo $white "https://chat.whatsapp.com/EfnBy4ikANGHIxmyWSkVSC"
 echo $white "Silakan Join Link GCnya"
 echo $white "Thanks"
 fi
+
 if [ $santai = "3" ]
 then
 figlet Selamat
